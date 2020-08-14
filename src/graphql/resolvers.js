bda5808c273427cd8907cbc4c965924b35fa106e
kelvynsantana <santanakelvyn@gmail.com>
2020-08-14 02:51:15 -0300

    Add Schemas and resolvers

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,3 +1,10 @@
-export default {
-  
-}
+import { fileLoader, mergeResolvers } from 'merge-graphql-schemas';
+import path from 'path';
+
+const resolversArray = fileLoader(path.join(__dirname, 'modules', '**', '*.resolvers.js'));
+
+const typeDefs = mergeResolvers(resolversArray);
+
+
+export default(typeDefs);
+
